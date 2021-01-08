bce50d4e00994f72a864d90ece65ea335ea5e46c
gaodan1008 <820506157@qq.com>
2021-01-08 21:37:56 +0800

    期末作业

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 
-#from .models import Article
+# Create your views here.
+from .models import Article
 
-'''
-#def year_archive(request, year):
-    #a_list = Article.objects.filter(pub_date__year=year)
-    #context = {'year': year, 'article_list': a_list}
-    #return render(request, 'news/year_archive.html', context)
-'''
+def year_archive(request, year):
+    a_list = Article.objects.filter(pub_date__year=year)
+    context = {'year': year, 'article_list': a_list}
+    return render(request, 'news/year_archive.html', context)
 
 from .models import Student, Homework
 
